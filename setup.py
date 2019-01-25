c4823efc4b8e5f187a64f8a4e9962e328bf86967
Rodolfo De Nadai <rdenadai@gmail.com>
2019-01-25 11:23:13 -0200

    criado diretório dos notebooks

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ia870',
+      version='0.1',
+      description='IA870 Python Morphology Toolbox',
+      url='https://github.com/robertoalotufo/ia870p3',
+      author='Roberto Lotufo',
+      author_email='',
+      license='BSD 2',
+      packages=['ia870'],
+      zip_safe=False)
